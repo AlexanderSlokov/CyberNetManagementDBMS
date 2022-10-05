7460248f18ef98181d78b315fe73df16bf28e619
AlexanderSlokov <102212788+AlexanderSlokov@users.noreply.github.com>
2022-10-05 22:39:08 +0700

    Change In Add A Computer

--- CyberGamingManagement/AdminForm/AddComputerForm.Designer.cs
+++ CyberGamingManagement/AdminForm/AddComputerForm.Designer.cs
@@ -46,25 +46,29 @@
             // 
             this.comboBoxRoomID.Enabled = false;
             this.comboBoxRoomID.FormattingEnabled = true;
-            this.comboBoxRoomID.Location = new System.Drawing.Point(12, 62);
+            this.comboBoxRoomID.Location = new System.Drawing.Point(16, 76);
+            this.comboBoxRoomID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBoxRoomID.Name = "comboBoxRoomID";
-            this.comboBoxRoomID.Size = new System.Drawing.Size(286, 21);
+            this.comboBoxRoomID.Size = new System.Drawing.Size(256, 24);
             this.comboBoxRoomID.TabIndex = 70;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(90, 442);
+            this.label7.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.label7.Location = new System.Drawing.Point(120, 544);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.Size = new System.Drawing.Size(0, 16);
             this.label7.TabIndex = 68;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(95, 323);
+            this.label8.Location = new System.Drawing.Point(127, 398);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.Size = new System.Drawing.Size(0, 16);
             this.label8.TabIndex = 67;
             // 
             // labelExit
@@ -73,11 +77,12 @@
             this.labelExit.AutoEllipsis = true;
             this.labelExit.AutoSize = true;
             this.labelExit.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.labelExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelExit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(86)))), ((int)(((byte)(174)))));
-            this.labelExit.Location = new System.Drawing.Point(145, 515);
+            this.labelExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelExit.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.labelExit.Location = new System.Drawing.Point(126, 627);
+            this.labelExit.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelExit.Name = "labelExit";
-            this.labelExit.Size = new System.Drawing.Size(28, 16);
+            this.labelExit.Size = new System.Drawing.Size(37, 20);
             this.labelExit.TabIndex = 66;
             this.labelExit.Text = "Exit";
             this.labelExit.Click += new System.EventHandler(this.labelExit_Click);
@@ -87,22 +92,24 @@
             this.textBoxInfo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(233)))));
             this.textBoxInfo.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBoxInfo.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxInfo.Location = new System.Drawing.Point(12, 199);
+            this.textBoxInfo.Location = new System.Drawing.Point(16, 245);
+            this.textBoxInfo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxInfo.Multiline = true;
             this.textBoxInfo.Name = "textBoxInfo";
-            this.textBoxInfo.Size = new System.Drawing.Size(289, 197);
+            this.textBoxInfo.Size = new System.Drawing.Size(256, 242);
             this.textBoxInfo.TabIndex = 65;
             // 
             // buttonAdd
             // 
-            this.buttonAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(86)))), ((int)(((byte)(174)))));
+            this.buttonAdd.BackColor = System.Drawing.Color.White;
             this.buttonAdd.Cursor = System.Windows.Forms.Cursors.Hand;
             this.buttonAdd.FlatAppearance.BorderSize = 0;
             this.buttonAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonAdd.ForeColor = System.Drawing.Color.White;
-            this.buttonAdd.Location = new System.Drawing.Point(18, 420);
+            this.buttonAdd.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.buttonAdd.Location = new System.Drawing.Point(16, 513);
+            this.buttonAdd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonAdd.Name = "buttonAdd";
-            this.buttonAdd.Size = new System.Drawing.Size(289, 35);
+            this.buttonAdd.Size = new System.Drawing.Size(256, 43);
             this.buttonAdd.TabIndex = 59;
             this.buttonAdd.Text = "ADD";
             this.buttonAdd.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
@@ -113,21 +120,23 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(86)))), ((int)(((byte)(174)))));
-            this.label1.Location = new System.Drawing.Point(97, 9);
+            this.label1.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(131, 27);
+            this.label1.Size = new System.Drawing.Size(267, 34);
             this.label1.TabIndex = 55;
-            this.label1.Text = "Computer";
+            this.label1.Text = "Add A Computer";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold);
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(165)))), ((int)(((byte)(169)))));
-            this.label3.Location = new System.Drawing.Point(12, 42);
+            this.label3.Location = new System.Drawing.Point(16, 52);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(109, 17);
+            this.label3.Size = new System.Drawing.Size(143, 23);
             this.label3.TabIndex = 72;
             this.label3.Text = "Computer Room";
             // 
@@ -136,9 +145,10 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold);
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(165)))), ((int)(((byte)(169)))));
-            this.label4.Location = new System.Drawing.Point(16, 161);
+            this.label4.Location = new System.Drawing.Point(19, 197);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(79, 17);
+            this.label4.Size = new System.Drawing.Size(102, 23);
             this.label4.TabIndex = 73;
             this.label4.Text = "Description";
             // 
@@ -147,18 +157,20 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold);
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(165)))), ((int)(((byte)(169)))));
-            this.label2.Location = new System.Drawing.Point(14, 99);
+            this.label2.Location = new System.Drawing.Point(19, 122);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 17);
+            this.label2.Size = new System.Drawing.Size(85, 23);
             this.label2.TabIndex = 75;
             this.label2.Text = "Bulk Add";
             // 
             // comboBoxBulkAdd
             // 
             this.comboBoxBulkAdd.FormattingEnabled = true;
-            this.comboBoxBulkAdd.Location = new System.Drawing.Point(12, 119);
+            this.comboBoxBulkAdd.Location = new System.Drawing.Point(16, 146);
+            this.comboBoxBulkAdd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBoxBulkAdd.Name = "comboBoxBulkAdd";
-            this.comboBoxBulkAdd.Size = new System.Drawing.Size(286, 21);
+            this.comboBoxBulkAdd.Size = new System.Drawing.Size(256, 24);
             this.comboBoxBulkAdd.TabIndex = 76;
             // 
             // buttonAssignLocalPC
@@ -166,20 +178,20 @@
             this.buttonAssignLocalPC.BackColor = System.Drawing.Color.White;
             this.buttonAssignLocalPC.Cursor = System.Windows.Forms.Cursors.Hand;
             this.buttonAssignLocalPC.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonAssignLocalPC.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(86)))), ((int)(((byte)(174)))));
-            this.buttonAssignLocalPC.Location = new System.Drawing.Point(18, 462);
-            this.buttonAssignLocalPC.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonAssignLocalPC.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.buttonAssignLocalPC.Location = new System.Drawing.Point(16, 565);
+            this.buttonAssignLocalPC.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.buttonAssignLocalPC.Name = "buttonAssignLocalPC";
-            this.buttonAssignLocalPC.Size = new System.Drawing.Size(289, 46);
+            this.buttonAssignLocalPC.Size = new System.Drawing.Size(256, 57);
             this.buttonAssignLocalPC.TabIndex = 77;
             this.buttonAssignLocalPC.Text = "Assign This PC";
             this.buttonAssignLocalPC.UseVisualStyleBackColor = false;
             // 
             // AddNewComputerForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(324, 549);
+            this.ClientSize = new System.Drawing.Size(292, 676);
             this.Controls.Add(this.buttonAssignLocalPC);
             this.Controls.Add(this.comboBoxBulkAdd);
             this.Controls.Add(this.label2);
@@ -192,6 +204,7 @@
             this.Controls.Add(this.textBoxInfo);
             this.Controls.Add(this.buttonAdd);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "AddNewComputerForm";
             this.Text = "Add New Computer";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.AddNewComputerForm_FormClosed);
